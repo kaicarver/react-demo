924044386670fc179810991f32cca42562f0bdc4
Kai Carver <kaicarver@gmail.com>
2020-02-25 19:11:08 +0100

    just a small 你好世界

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          你好世界!
         </p>
         <a
           className="App-link"
